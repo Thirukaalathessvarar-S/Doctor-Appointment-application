ac95aa3fb26514094f88114e5305f0033d25667c
Thirukaalathessvarar S <121166390+Thirukaalathessvarar-S@users.noreply.github.com>
2024-07-18 15:14:16 +0530

    Update urls.py

--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from myapp import views
 urlpatterns=[
-    path('',views.index,name='index'),
+    path('Doctor-Appointment-application/',views.index,name='index'),
     path('login/',views.login,name='login'),
     path('signup/',views.signup,name='signup'),
    path('home/',views.home_view,name='home'),
